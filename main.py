a9c0f39859ff27f1e22ab5eb8e01cbf7ad19a6a9
GarfieldMa <garfie1d@connect.hku.hk>
2017-06-21 01:44:00 +0800

    update parameters

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
      u_term, v_term, mu_term, t_term, var_terms,
      dig_h, toc1, toc2, T1, T2,
      Pr, Psi_s, Ns, err, wall_time) = builder(nmax=1, t_first=0.2, t_second=0.4,
-                                              err=1.0e-5, n1=6, n2=6, mu_range=1.5, ma=6, wall_time=50)
+                                              err=1.0e-5, n1=15, n2=8, mu_range=1.5, ma=12, wall_time=10)
 
     Psi_s, Ns = solves(nmax, hexagon_mf_bases, hexagon_mf_operators,
                        t_a, t_b, tA, ts, Ma,
